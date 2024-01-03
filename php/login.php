9283abec6cce906920190de7dd0e8895d98cffc7
ihawp <ihawpebay@gmail.com>
2024-01-02 18:07:42 -0600

    added page tracking to redirects!

--- php/login.php
+++ php/login.php
@@ -13,12 +13,9 @@ if (isset($_POST['loginusername']) && isset($_POST['loginpassword'])) {
     $stmt = $conn->prepare('SELECT password, id, username FROM accounts WHERE username = ?');
     $stmt->bind_param('s', $username);
 
-    // Execute the query
     if ($stmt->execute()) {
-        // Bind result variables
         $stmt->bind_result($passwordResult, $idResult, $usernameResult);
 
-        // Fetch the result
         if ($stmt->fetch()) {
             if (password_verify($password, $passwordResult)) {
                 $_SESSION['username'] = $usernameResult;
@@ -34,7 +31,6 @@ if (isset($_POST['loginusername']) && isset($_POST['loginpassword'])) {
             exit();
         }
     } else {
-        // Handle the case where the query execution failed
         header('Location: ../index.html?error=queryerror&page=login');
         exit();
     }
