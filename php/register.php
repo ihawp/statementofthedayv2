01f6afda00d3289014ad1933a389df5e54a85849
ihawp <ihawpebay@gmail.com>
2024-01-02 18:05:49 -0600

    added page tracking to redirects!

--- php/register.php
+++ php/register.php
@@ -10,7 +10,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
 
-    // Check if the username already exists in the database
     $stmtCheckUsername = $conn->prepare('SELECT username FROM accounts WHERE username = ?');
     $stmtCheckUsername->bind_param('s', $username);
 
@@ -18,24 +17,18 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         $stmtCheckUsername->store_result();
 
         if ($stmtCheckUsername->num_rows > 0) {
-            // Username already in use
             $stmtCheckUsername->close();
-            header('Location: ../index.html?error=usernamealreadyinuse');
+            header('Location: ../index.html?error=usernamealreadyinuse&page=register');
             exit();
         }
     } else {
-        // Handle the case where the query execution failed
         $stmtCheckUsername->close();
-        header('Location: ../index.html?error=queryerror');
+        header('Location: ../index.html?error=queryerror&page=register');
         exit();
     }
 
     $stmtCheckUsername->close();
-
-    // Hash the password
     $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-    // Insert new user into the database
     $stmtInsertUser = $conn->prepare('INSERT INTO accounts (username, password, last_login, account_created) VALUES (?, ?, NOW(6), NOW(6))');
     $stmtInsertUser->bind_param('ss', $username, $hashedPassword);
 
@@ -43,16 +36,15 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         $_SESSION['username'] = $username;
         $_SESSION['user_id'] = getUserID($conn);
         $stmtInsertUser->close();
-        header('Location: ../index.html');
+        header('Location: ../index.html?page=register');
         exit();
     } else {
-        // Handle the case where the insertion failed
         $stmtInsertUser->close();
-        header('Location: ../index.html?error=insertionerror');
+        header('Location: ../index.html?error=insertionerror&page=register');
         exit();
     }
 } else {
-    header('Location: ../index.html');
+    header('Location: ../index.html?page=register');
     exit();
 }
 
