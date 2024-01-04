ed28f0b6b43476b566650d1210bb00d395c16268
ihawp <ihawpebay@gmail.com>
2024-01-04 17:27:42 -0600

    you can add comments now!

---
+++ php/addComment.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'db_conn.php';
+session_start();
+
+$parentID = htmlspecialchars($_POST['post_id']);
+$userID = htmlspecialchars($_SESSION['user_id']);
+$commentContent = htmlspecialchars($_POST['commentContent']);
+
+$stmt = $conn->prepare('INSERT INTO posts (parent_post_id, user_id, username, content, time_posted) VALUES (?, ?, ?, ?, NOW(6))');
+$stmt->bind_param('iiss', $parentID, $userID, $_SESSION['username'], $commentContent);
+if ($stmt->execute()) {
+    header('Location: ../index.html?');
+    exit();
+} else {
+    header('Location: ');
+    exit();
+}
