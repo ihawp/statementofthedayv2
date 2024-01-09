a04aa0bc8c00a916e60f0935a4de6665a47967a1
ihawp <ihawpebay@gmail.com>
2024-01-09 11:19:03 -0600

    checks if a user is following another user

---
+++ php/checkFollow.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'db_conn.php';
+session_start();
+
+$followedID = $_GET['followed_id'];
+$followerID = $_GET['follower_id'];
+
+$stmt = $conn->prepare('SELECT * FROM follows WHERE followed_id = ? AND following_id = ?');
+$stmt->bind_param('ii',$followedID, $followerID);
+if ($stmt->execute()) {
+    $stmt->store_result();
+    $r = $stmt->num_rows;
+    $stmt->close();
+    if ($r !== 0) {
+        echo json_encode(['following'=>true]);
+    } else {
+        echo json_encode(['following'=>false]);
+    }
+}
+
+$conn->close();
+exit();
