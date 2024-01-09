75bca7adea31d0610572677c3f2526477dc1a5f8
ihawp <ihawpebay@gmail.com>
2024-01-09 11:17:49 -0600

    removes original posts and all-level comments below the post, needs added security

--- php/removePost.php
+++ php/removePost.php
@@ -6,14 +6,29 @@ session_start();
 $userID = htmlspecialchars($_GET['user_id']);
 $postID = htmlspecialchars($_GET['post_id']);
 
-$stmt = $conn->prepare('DELETE FROM posts WHERE post_id = ? AND user_id = ?');
-$stmt->bind_param('ii', $postID, $_SESSION['user_id']);
+// Check if it's a comment
+$stmtCheckComment = $conn->prepare('SELECT parent_post_id FROM posts WHERE post_id = ? LIMIT 1');
+$stmtCheckComment->bind_param('i', $postID);
+$stmtCheckComment->execute();
+$resultCheckComment = $stmtCheckComment->get_result();
+$rowCheckComment = $resultCheckComment->fetch_assoc();
+
+if ($rowCheckComment && $rowCheckComment['parent_post_id'] !== null) {
+    $parentPostID = $rowCheckComment['parent_post_id'];
+    $stmtDecrementCommentCount = $conn->prepare('UPDATE posts SET comments = comments - 1 WHERE post_id = ?');
+    $stmtDecrementCommentCount->bind_param('i', $parentPostID);
+    $stmtDecrementCommentCount->execute();
+}
+
+$stmt = $conn->prepare('DELETE FROM posts WHERE post_id = ? AND user_id = ? OR parent_post_id = ? OR super_parent_post_id = ?');
+$stmt->bind_param('iiii', $postID, $_SESSION['user_id'], $postID, $postID);
+
 if (intval($userID) === $_SESSION['user_id']) {
     if ($stmt->execute()) {
-        echo json_encode(['post_deleted'=>true]);
+        echo json_encode(['post_deleted' => true]);
     } else {
-        echo json_encode(['stmt'=>false]);
+        echo json_encode(['stmt' => false]);
     }
 } else {
-    echo json_encode(['stmt'=>$userID, 'stmtt'=>$_SESSION['user_id']]);
+    echo json_encode(['stmt' => $userID, 'stmtt' => $_SESSION['user_id']]);
 }
